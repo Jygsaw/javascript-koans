49b1d33fedadb6059245083d32df142ddc4fd586
Jygsaw <jygsaw@gmail.com>
2014-05-06 21:21:34 -0700

    Complete pizza koans AboutApplyingWhatWeHaveLearnt

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -32,7 +32,7 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
@@ -40,8 +40,16 @@ describe("About Applying What We Have Learnt", function() {
       var productsICanEat = [];
 
       /* solve using filter() & all() / any() */
+      _(products).filter(function(pizza) {
+        if (!pizza.containsNuts &&
+            !_(pizza.ingredients).any(function(ingredient) {
+            return ingredient === "mushrooms" ? true : false;
+          })) {
+          productsICanEat.push(this);
+        }
+      });
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
